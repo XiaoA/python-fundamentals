16ce706d94c0ba1124f57a87192cc092e3103812
Andrew Buckingham <abuckingham99@gmail.com>
2021-02-03 21:52:17 -0800

    Step One: Exercise #1

---
+++ words.py
@@ -0,0 +1,16 @@
+def print_upper_words(words):
+    """ Print one word per line, uppercased.
+
+    print_upper_words(["I" "need" "a" "cup" "of" "coffee" "now"])
+    I
+    NEED
+    A
+    CUP 
+    OF 
+    COFFEE
+    NOW
+
+    """
+
+    for word in words:
+        print(word.upper())
